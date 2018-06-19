8e65558a0503a03f10fde4d8507c1b0f7e4add09
Philip Steiner <philip@psteiner.com>
2018-06-18 22:33:00 -0700

    meadow update algo improvements

--- assets/js/island.js
+++ assets/js/island.js
@@ -6,14 +6,26 @@ var y = canvas.height - 30;
 var days = 0;
 
 // meadows
-var meadowRowCount = 3;
-var meadowColumnCount = 3;
+var meadowRowCount = 5;
+var meadowColumnCount = 5;
+var islandSize = meadowRowCount * meadowColumnCount;
 var meadowPadding = 2;
-var meadowWidth = 20;
-var meadowHeight = 20;
+var meadowWidth = 50;
+var meadowHeight = 50;
 var meadowOffsetLeft = (canvas.width - meadowRowCount * meadowWidth) / 2;
 var meadowOffsetTop = (canvas.height - meadowColumnCount * meadowHeight) / 2;
-var meadowStates = ["#9b7858","#999999","#219923"];
+
+//    forest: "#146357",
+// lava: "#FF7905",
+
+var meadowStates = [
+    "#666666", // rock
+    "#9b7858", // soil
+    "#219923", // grass
+]
+
+// "#71cbff", // water
+
 
 var meadows = [];
 
@@ -25,13 +37,14 @@ for (var c = 0; c < meadowColumnCount; c++) {
         meadows[c][r] = {
             x: 0,
             y: 0,
-            state: 0
+            state: 0,
+            age: 0
         };
     }
 }
 
 var cycles = 0;
-var cyclesPerDay = 60;
+var cyclesPerDay = 100;
 
 function draw() {
 
@@ -39,10 +52,10 @@ function draw() {
 
     drawMeadow();
     drawDays();
-    updateIsland();
 
     if (++cycles > cyclesPerDay) {
         days++;
+        updateIsland();
         cycles = 0;
     }
 
@@ -62,18 +75,30 @@ function drawMeadow() {
             ctx.fillStyle = meadowStates[meadow.state];
             ctx.fill();
             ctx.closePath();
+            ctx.font = "8pt Arial";
+            ctx.fillStyle = "white";
+            ctx.fillText("s:" + meadow.state + " f:" + meadow.factor, 
+                meadow.x + 10, meadow.y + meadowHeight / 2);
         }
     }
 }
 
 function updateIsland() {
+    var numberOfStates = Object.keys(meadowStates).length;
+
     for (var c = 0; c < meadowColumnCount; c++) {
         for (var r = 0; r < meadowRowCount; r++) {
             var meadow = meadows[c][r];
-            var numberOfStates = Object.keys(meadowStates).length;
-            var state = getRandomInt(numberOfStates);
-            meadow.state++;
 
+            // update each meadow
+            meadow.age++;
+            meadow.factor = getRandomInt(10);
+            var updateState = (meadow.age % meadow.factor) == 0;
+
+            if (updateState) {
+                var newState = (meadow.state + 1) % numberOfStates;
+                meadow.state = newState;
+            }
         }
     }
 }
