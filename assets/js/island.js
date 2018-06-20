ecf77d8d58b07b75f5b1300cd568327c7b7f832a
Philip Steiner <philip@psteiner.com>
2018-06-19 22:53:16 -0700

    Expand island and update cells

--- assets/js/island.js
+++ assets/js/island.js
@@ -1,35 +1,35 @@
 // playing field
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var x = canvas.width / 2;
-var y = canvas.height - 30;
 var days = 0;
 
 // meadows
-var meadowRowCount = 5;
-var meadowColumnCount = 5;
-var islandSize = meadowRowCount * meadowColumnCount;
-var meadowPadding = 2;
-var meadowWidth = 50;
-var meadowHeight = 50;
+var meadowRowCount = 20;
+var meadowColumnCount = 20;
+var meadowPadding = 1;
+var meadowWidth = 30;
+var meadowHeight = 30;
 var meadowOffsetLeft = (canvas.width - meadowRowCount * meadowWidth) / 2;
 var meadowOffsetTop = (canvas.height - meadowColumnCount * meadowHeight) / 2;
-var meadowStates = {
-    water: '#3355FF',
-    lava: '#ff4400',
-    rock: '#999999',
-    soil: '#9b7858',
-    grass: '#229922',
-};
+// var meadowStates = {
+//     water: '#3355FF',
+//     lava: '#ff4400',
+//     rock: '#999999',
+//     soil: '#9b7858',
+//     grass: '#229922',
+// };
 
+// var meadowStatesIndexed = [
+//     { lava: '#ff4400' },
+//     { rock: '#999999' },
+//     { soil: '#9b7858' },
+//     { grass: '#229922' },
+//     { water: '#3355FF' }
+// ];
 
-var meadowStatesIndexed = [
-    { lava: '#ff4400' },
-    { rock: '#999999' },
-    { soil: '#9b7858' },
-    { grass: '#229922' },
-    { water: '#3355FF' }
-];
+//                   water,    lava,     rock,     soil,     grass
+var meadowStates = ["#3355FF","#ff4400","#999999","#9b7858","#229922"];
+
 var meadows = [];
 
 // initialize the meadow objects
@@ -40,13 +40,16 @@ for (var c = 0; c < meadowColumnCount; c++) {
         meadows[c][r] = {
             x: 0,
             y: 0,
-            state: 'water'
+            state: 0,
+            age: 0,
+            factor: 0,
+            reset: 0
         };
     }
 }
 
 var cycles = 0;
-var cyclesPerDay = 100;
+var cyclesPerDay = 10;
 
 function draw() {
 
@@ -76,30 +79,40 @@ function drawMeadow() {
             ctx.fillStyle = meadowStates[meadow.state];
             ctx.fill();
             ctx.closePath();
-            ctx.font = "8pt Arial";
-            ctx.fillStyle = "white";
-            ctx.fillText("s:" + meadow.state + " f:" + meadow.factor, 
-                meadow.x + 10, meadow.y + meadowHeight / 2);
+            // ctx.font = "8pt Arial";
+            // ctx.fillStyle = "black";
+            // ctx.fillText("s:" + meadow.state + " f:" + meadow.factor, 
+            //     meadow.x + 2, meadow.y + 20);
+            if ( meadow.reset == 1 ) {
+                ctx.font = "12pt Arial";
+                ctx.fillStyle = "yellow";
+                ctx.fillText("!!!",meadow.x + 10, meadow.y + 20);
+            }
         }
     }
 }
 
 function updateIsland() {
-    var numberOfStates = Object.keys(meadowStates).length;
-
+    var states = meadowStates.length - 1;
     for (var c = 0; c < meadowColumnCount; c++) {
         for (var r = 0; r < meadowRowCount; r++) {
-            var meadow = meadows[c][r];
             
-
-            // update each meadow
+            var meadow = meadows[c][r];
+            meadow.reset = 0;
             meadow.age++;
-            meadow.factor = getRandomInt(10);
+            
+            meadow.factor = getRandomInt(293);
+
             var updateState = (meadow.age % meadow.factor) == 0;
 
-            if (updateState) {
-                var newState = (meadow.state + 1) % numberOfStates;
-                meadow.state = newState;
+            if (updateState && meadow.state < states) {
+                meadow.state++;
+            }
+
+            // random reset back to lava!
+            if (getRandomInt(1000) == 0) {
+                meadow.state = 1;
+                meadow.reset = 1;
             }
         }
     }
