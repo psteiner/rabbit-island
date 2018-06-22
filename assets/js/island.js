7f2d28eb0ab9e4b0fb90c272ed370e41335d7d38
Philip Steiner <philip@psteiner.com>
2018-06-21 22:23:14 -0700

    add createIsland()

--- assets/js/island.js
+++ assets/js/island.js
@@ -4,8 +4,8 @@ var ctx = canvas.getContext("2d");
 var days = 0;
 
 // cels
-var celRows = 10;
-var celCols = 10;
+var celRows = 15;
+var celCols = 15;
 var celPadding = 1;
 var celWidth = 30;
 var celHeight = 30;
@@ -24,12 +24,14 @@ const fire = 6;
 const lake = 7;
 
 // celStates.length => 8
-var celStates = ["#3355FF", "#FFFFFF", "#ff4400", "#999999", "#9b7858", "#229922", "ff9102", "7fddff"];
+var celStates = ["#3355FF", "#FFFFFF", "#FF4400", "#999999", "#9B7858", "#229922", "#FF9102", "#7FDDFF"];
 
+
+
+// initialize the island map
+//
 var cels = [];
 
-// initialize the cel objects
-//
 for (var c = 0; c < celCols; c++) {
     cels[c] = [];
     for (var r = 0; r < celRows; r++) {
@@ -37,18 +39,24 @@ for (var c = 0; c < celCols; c++) {
             x: 0,
             y: 0,
             z: 0,
-            state: ocean,
+            state: lake,
             age: 0,
-            update: 0,
             burning: 0
         };
     }
 }
 
+// leave the corner cels as ocean
+// how much corner depends on how big the map
+var minCol = Math.round(celCols * 0.1);
+var maxCol = Math.round(celCols * 0.9);
+var minRow = Math.round((celRows - 1) * 0.1);
+var maxRow = Math.round((celRows - 1) * 0.9);
+
 var cycles = 0;
 var cyclesPerDay = 10;
 
-function draw() {
+function runSim() {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -61,7 +69,7 @@ function draw() {
         cycles = 0;
     }
 
-    requestAnimationFrame(draw);
+    requestAnimationFrame(runSim);
 }
 
 function drawIsland() {
@@ -90,9 +98,8 @@ function drawIsland() {
 }
 
 function updateIsland() {
-    var endState = celStates.length - 1;
-    for (var c = 0; c < celCols; c++) {
-        for (var r = 0; r < celRows; r++) {
+    for (var c = minCol; c < maxCol; c++) {
+        for (var r = minRow; r < maxRow; r++) {
 
             var cel = cels[c][r];
             cel.burning = 0;
@@ -100,11 +107,11 @@ function updateIsland() {
 
             // flag to allow updating the cel to the next state
             //
-            cel.update = cel.age % getRandomInt(293) == 0;
+            var update = cel.age % getRandomInt(293) == 0;
 
             // progress cell state from ocean to grass, not late
             //
-            if (cel.update && cel.state < endState - 1) {
+            if (update && cel.state < grass) {
                 cel.state++;
             }
 
@@ -138,8 +145,60 @@ function updateIsland() {
     }
 }
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+
+function createIsland() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    generateMap();
+    drawIsland();
+    drawDays();
+}
+
+
+// populates the island map with its initial geography
+// 
+function generateMap() {
+    // create the island shape - first start with a random cel
+    //                 0 .. 9                  0 .. 9
+    // var hotspot = cels[getRandomInt(celCols)][getRandomInt(celRows)];
+    
+    // iterate over the map cells, excluding the ocean corners
+    // getRandomInt(minCol, celCols)
+    // getRandomInt(minRow, celRows)
+    for (var c = minCol; c < maxCol; c++) {
+        for (var r = minRow; r < maxRow; r++) {
+
+            // default to grass for now to see if this works or not!
+            var cel = cels[c][r].state = grass;
+
+            // do something with the neighbouring cels
+            // https://stackoverflow.com/questions/652106/finding-neighbours-in-a-two-dimensional-array
+            //
+            // for (var n = Math.max(0, c - 1); n <= Math.min(c + 1, celCols - 1); n++) {
+            //     for (var m = Math.max(0, r - 1); m <= Math.min(r + 1, celRows - 1); m++) {
+            //         if (n !== c || m !== r) {
+            //             // console.log("c:" + c + " r:" + r);
+            //             // console.log("n:" + n + " m:" + m);
+            //             if (cel.state === 1) {
+            //                 //cels[n][m].state = cel.state;
+            //             }
+            //         }
+            //     }
+            // }
+
+        }
+    }    
+}
+
+// returns an integer between min and less than but not including max
+// e.g. getRandomInt(3) -> 0, 1, or 2
+//      getRandomInt(3, 7) -> 3, 4, 5, or 6
+//
+// Source https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+//
+function getRandomInt(min = 0, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function drawDays() {
@@ -148,4 +207,5 @@ function drawDays() {
     ctx.fillText("Days: " + days, 10, canvas.height - 30);
 }
 
-draw();
+createIsland();
+runSim();
